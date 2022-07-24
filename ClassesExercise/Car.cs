07a802b011a8fb82646aa2a5ed3a747bba060c6d
kmpark2 <98770234+kmpark2@users.noreply.github.com>
2022-07-24 19:22:32 +0900

    initial commit

--- ClassesExercise/Car.cs
+++ ClassesExercise/Car.cs
@@ -13,5 +13,12 @@ namespace ClassesExercise
         public string Make{ get; set; }
         public string Model{ get; set; }
         public int Year { get; set; }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
     }
 }
--- ClassesExercise/Program.cs
+++ ClassesExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesExercise
 {
@@ -11,9 +12,24 @@ namespace ClassesExercise
             myCar.Model = "Sonata";
             myCar.Year = 2002;
 
-            Console.WriteLine($"Make: {myCar.Make}");
-            Console.WriteLine($"Model: {myCar.Model}");
-            Console.WriteLine($"Year: {myCar.Year}");
+            Car yourCar = new Car("Kia", "Forte", 2015);
+
+            Car herCar = new Car()
+            {
+                Make = "Toyota",
+                Model = "Camry",
+                Year = 2020
+            };
+
+            var carList = new List<Car>();
+            carList.Add(myCar);
+            carList.Add(yourCar);
+            carList.Add(herCar);
+            
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
         }
     }
 }
